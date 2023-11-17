454c268227fb820cdab39f2479f0b4b46bc9ffe0
yel <118788188+YelMariel@users.noreply.github.com>
2023-11-17 08:17:12 +0800

    Update admin.jsx

--- components/dashboard/admin.jsx
+++ components/dashboard/admin.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useRouter } from "next/router";
-import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ToggleSwitch from './switch'; // Adjust the path to the actual location of your ToggleSwitch component
-
-
+import Disp from '../../components/dispenser';
 import styles from "../../styles/bg.module.css"; // Adjust the relative path to match your project structure
+import Change from '../../components/postreq';
+import Logs from '../../components/inventory';
+import Adding from '../../components/Adddispensermodal'
 
-  
 
 
   const loadSavedTexts = () => {
@@ -52,25 +51,6 @@ function DateTimeComponent() {
 }
 
 export default function Dashboard({ user }) {
-  const [fetchedData, setFetchedData] = useState([]);
-
-  useEffect(() => {
-    console.log('Before fetching data');
-    axios.get('http://localhost:3003/status')
-      .then(response => {
-        console.log('After fetching data');
-        console.log('Response:', response.data); // Log the response data
-        setFetchedData(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data from the API:', error);
-      });
-  }, []);
-  
-  
-  
-  
-  
   const dateTimeContainerStyle = {
     position: 'relative',
     top: '20px',
@@ -98,15 +78,15 @@ export default function Dashboard({ user }) {
   const [showModal, setShowModal] = useState(false); // State to control the modal visibility
   const [inputText, setInputText] = useState(''); // State to store user input
   const [savedTexts, setSavedTexts] = useState([]); // State to store entered texts
-  const [displayText, setDisplayText] = useState(''); // State to store the displayed text
-  const [isInputFilled, setIsInputFilled] = useState(false);
   const [maxRefills, setMaxRefills] = useState(0); // State to store the maximum refills
+  const [showRegisterBox, setShowRegisterBox] = useState(false);
 
-  useEffect(() => {
-    // Load saved texts from local storage when the component mounts
-    const loadedSavedTexts = loadSavedTexts();
-    setSavedTexts(loadedSavedTexts);
-  }, []);
+  const handleRegisterButtonClick = () => {
+    // Perform your registration logic here
+    // For now, let's just toggle the visibility of the box
+    setShowRegisterBox(!showRegisterBox);
+
+  };
 
   // Function to save the entered text to local storage
   const saveTextToLocalStorage = (texts) => {
@@ -119,22 +99,10 @@ export default function Dashboard({ user }) {
     console.log('Set maximum refills:', maxRefills);
   };
 
-  const hoverStyles = {
-    fontWeight: 'bold',
-  };
-
-  const customBorderStyle = {
-    border: '1px solid #000', // Black border with 1px width
-  };
-
+ 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
   };
-
-  const handleModalClose = () => {
-    setShowModal(false);
-  };
-
 
 
 
@@ -157,7 +125,7 @@ export default function Dashboard({ user }) {
     setShowModal(false);
   };
 
-  const renderSavedTexts = () => {
+  const renderSavedTexts = () => { 
     const outerBoxStyle = {
       width: 'auto', // Adjust the width as needed
       height: 'auto', // Adjust the height as needed
@@ -167,139 +135,27 @@ export default function Dashboard({ user }) {
       margin: '10px', // Adjust the margin as needed
       borderRadius: '15px', // Adjust the borderRadius to your desired value
     };
-  
-    const innerBoxStyle = {
-      width: '100%', // Make the inner box fill the outer box
-      height: 'auto', // Make the inner box fill the outer box
-      border: '1px solid #000',
-      padding: '10px',
-      backgroundColor: '#928C8C', // Adjust the background color as needed
-      overflow: 'auto', // Add scrollbars if needed for long text
-    };
-  
-    const textContainerStyle = {
-      textAlign: 'center',
-      fontSize: '16px', // Adjust font size
-      fontWeight: 'bold', // Adjust font weight
-      color: 'white', // Adjust text color
-    };
-  
-    const statusTextStyle = {
-      textAlign: 'center',
-      fontSize: '15px', // Adjust font size
-      fontWeight: 'bold', // Adjust font weight
-      color: 'black', // Adjust text color
-    };
-  
-    const buttonStyle = {
-      marginLeft: 'auto',
-      bottom: '10px', // Adjust the distance from the bottom as needed
-      position: 'relative',
-    };
-  
-    const additionalTextStyle = {
-      display: 'flex',
-      alignItems: 'center',
-      marginTop: '20px',
-      paddingTop: '10px', // Add padding for spacing
-    };
-  
-    const boxInsideStyle = {
-      width: '50px', // Adjust the width as needed
-      height: '40px', // Adjust the height as needed
-      border: '1px solid #000',
-      backgroundColor: 'white',
-    };
-  
-    return savedTexts.map((text, index) => (
-      <div key={index} className="col-md-4 mb-3">
-        <div style={outerBoxStyle}>
-          <div style={innerBoxStyle}>
-            <div style={textContainerStyle}>{text}</div>
-          </div>
-          <p style={statusTextStyle}>STATUS</p>
-          <div style={additionalTextStyle}>
-            <p style={{ color: 'black' }}>Remaining:</p>
-            <div style={boxInsideStyle}>
-              <p style={{ color: 'black' }}>{maxRefills}</p> {/* Display maximum refills here */}
-            </div>
-            <button
-              className="btn btn-danger btn-sm mt-2"
-              onClick={() => handleDeleteText(index)}
-              style={buttonStyle}
-            >
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
-    ));
   };
-  
-
-  
-  
-  
 
   const renderTabContent = () => {
     switch (activeTab) {
       case 'Dashboard':
         return (
           <div className="row">
+            <Disp /> 
             {renderSavedTexts()}
-            <div className="col-md-4 mb-3">
-              <button
-                className="btn btn-primary btn-sm mt-2"
-                style={{
-                  width: '100px',  // Fixed width
-                  height: '100px',  // Fixed height
-                }}
-                onClick={() => setShowModal(true)}
-              >
-                Add Dispenser
-              </button>
-            </div>
-            {fetchedData.map((item) => (
-                <div key={item.id}>
-                  <p>ID: {item.id}</p>
-                  <p>Water Level: {item.water_level}</p>
-                </div>
-              ))}
-
-          </div>
+            <Adding/>
+       </div>
         );
 
   case 'RefillLogs':
   return (
     <div>
-      <h2 className="fw-bold fs-5">Refill Logs</h2>
-      <div style={{ maxHeight: '200px', overflowY: 'auto' }}>
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Starting</th>
-              <th>Total Refills</th>
-              <th>Remaining Containers</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>2023-10-20</td>
-              <td>User 1</td>
-              <td>Dispenser 1</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>2023-10-19</td>
-              <td>User 2</td>
-              <td>Dispenser 2</td>
-              <td>30</td>
-            </tr>
-            
-            {/* Add more rows as needed */}
-          </tbody>
-        </table>
+      <h2 className="fw-bold fs-10">Refill Logs</h2>
+      <Logs/>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <button>ADD WATER CONTAINER</button>
+        <button>Inventory</button>
       </div>
     </div>
   );
@@ -325,6 +181,7 @@ export default function Dashboard({ user }) {
             >
               Change 
             </button>
+            <Change/>
           </div>
         );   
         case 'Switch':
@@ -410,77 +267,14 @@ export default function Dashboard({ user }) {
             <div style={{ marginLeft: '50px' }}>
               {renderTabContent()}
             </div>
-            {activeTab === 'Dashboard' && (
-              <div>
-                {fetchedData.map((item) => (
-                  <div key={item.id}>
-                    <p>ID: {item.id}</p>
-                    <p>Water Level: {item.water_level}</p>
-                  </div>
-                ))}
-              </div>
-            )}
+            {activeTab === 'Dashboard' 
+            
+            }
          
           </div>
         </div>
       </div>
-      {/* Modal for adding text */}
-      {showModal && (
-        <div className={`modal fade show`} tabIndex="-1" role="dialog" style={{ display: 'block' }}>
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-              <h5 className="modal-title" style={{ color: 'black' }}>Add Dispenser</h5>
-                <button
-                  type="button"
-                  className="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                  onClick={handleModalClose}
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <div className="form-group">
-                <label htmlFor="inputText" style={{ color: 'black' }}>Enter location:</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="inputText"
-                    value={inputText}
-                    onChange={(e) => setInputText(e.target.value)}
-                    style={{ color: 'black' }}
-                  />
-                </div>
-              </div>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  onClick={handleModalClose}
-                >
-                  Close
-                </button>
-                <button
-                type="button"
-                className="btn btn-primary"
-                onClick={() => {
-                  if (inputText) {
-                    handleSaveText();
-                    handleModalClose(); // Close the modal
-                  }
-                }}
-                disabled={!inputText} // Disable the button if the input field is not filled
-              >
-                Update
-              </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-      {showModal && <div className={`modal-backdrop fade show`}></div>}
+
     </div>
   );
 }
