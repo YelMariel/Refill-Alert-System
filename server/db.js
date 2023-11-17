51c3417c17d31aa50e27d5ca520b75c2abc5ceb5
yel <118788188+YelMariel@users.noreply.github.com>
2023-11-17 08:19:30 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,40 +1,45 @@
 const express = require('express');
-const mysql = require('mysql2'); // Use the 'mysql2/promise' package for async/await support
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-app.use(express.json());
+const port = 3001;
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'mariel',
-  database: 'db_alert',
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'mariel',
+    database: 'db_alert',
 });
 
-// Define an API endpoint to insert data into the database
-app.post('/api/store-location', async (req, res) => {
-  try {
-    const { location } = req.body;
-
-    // Get a connection from the pool
-    const connection = await pool.getConnection();
-
-    // Insert the data into the database
-    const [results] = await connection.execute('INSERT INTO locations (location) VALUES (?)', [location]);
-
-    // Release the connection back to the pool
-    connection.release();
-
-    // Respond with a success message
-    res.json({ message: 'Location data stored successfully' });
-  } catch (error) {
-    console.error('Error storing location data:', error);
-    res.status(500).json({ error: 'An error occurred while storing location data' });
-  }
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+    } else {
+        console.log('Connected to MySQL!');
+    }
 });
 
-const PORT = 3001 ;
+app.use(cors());
+app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.post('/register', (req, res) => {
+  const { ip_address } = req.body;
+
+  db.query('INSERT INTO users (ip_address) VALUES (?)', [ip_address], (err, results) => {
+      if (err) {
+          console.error('Error inserting data:', err);
+          res.status(500).send('Error inserting data into the database.');
+      } else {
+          console.log('IP has been registered successfully!');
+          res.status(200).send('IP has been registered successfully!');
+      }
+  });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
