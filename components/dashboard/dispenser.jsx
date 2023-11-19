95a106d4b146e86ab2722dd6f4dd8aa029e27b61
yel <118788188+YelMariel@users.noreply.github.com>
2023-11-19 15:34:48 +0800

    Update dispenser.jsx

--- components/dashboard/dispenser.jsx
+++ components/dashboard/dispenser.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [isLoading, setLoading] = useState(false);
-  const [prevData, setPrevData] = useState(null);
+  const [prevData, setPrevData] = useState([]);
 
   const fetchData = () => {
     setLoading(true);
@@ -32,50 +32,58 @@ export default function Home() {
     return () => clearInterval(intervalId);
   }, []);
 
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No data found</p>;
+  // Function to render data for a specific ID
+  const renderDataForId = (id) => {
+    const filteredData = data.filter((value) => value.id === id);
 
-  const filteredData = data.filter((value) => value.id === 1);
+    return (
+      <div key={`container-${id}`}>
+        <h2 className="fw-bold fs-10">Water Level (ID {id})</h2>
+        {filteredData.map((value) => (
+          <div key={value.id}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <p>HIGH</p>
+              <div
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  backgroundColor:
+                    value.water_level === "HIGH" ? "green" : "transparent",
+                  borderRadius: "50%",
+                  marginRight: "50px",
+                  marginLeft: "50px",
+                  border: "1px solid black",
+                }}
+              ></div>
+              <p>LOW</p>
+              <div
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  backgroundColor:
+                    value.water_level === "LOW" ? "red" : "transparent",
+                  borderRadius: "50%",
+                  border: "1px solid black",
+                }}
+              ></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  // Extract unique ids from the data
+  const uniqueIds = [...new Set(data.map((item) => item.id))];
 
   return (
     <div>
-      {filteredData.map((value) => (
-        <div key={value.id}>
-          <h2 className="fw-bold fs-10">Water Level</h2>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <p>HIGH</p>
-            <div
-              style={{
-                width: "30px",
-                height: "30px",
-                backgroundColor:
-                  value.water_level === "HIGH" ? "green" : "transparent",
-                borderRadius: "50%",
-                marginRight: "50px",
-                marginLeft: "50px",
-                border: "1px solid black",
-              }}
-            ></div>
-             <p>LOW</p>
-            <div
-              style={{
-                width: "30px",
-                height: "30px",
-                backgroundColor:
-                  value.water_level === "LOW" ? "red" : "transparent",
-                borderRadius: "50%",
-                border: "1px solid black",
-              }}
-            ></div>
-          </div>
-        </div>
-      ))}
+      {uniqueIds.map((id) => renderDataForId(id))}
     </div>
   );
 }
-  
