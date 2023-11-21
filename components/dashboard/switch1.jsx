8f1d4180cf7063985154a35eb9cd6b947edc0efe
yel <118788188+YelMariel@users.noreply.github.com>
2023-11-21 14:45:16 +0800

    Update switch1.jsx

--- components/dashboard/switch1.jsx
+++ components/dashboard/switch1.jsx
@@ -1,15 +1,35 @@
 // src/Settings.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Settings = () => {
   const [limit, setLimit] = useState(3);
   const [switchState, setSwitchState] = useState(false);
   const [time, setTime] = useState(0);
+  const [ipAddress, setIpAddress] = useState('');
+
+  useEffect(() => {
+    const fetchIpAddress = async () => {
+      try {
+        const response = await axios.get('http://localhost:3002/stuffs');
+        // Assuming the response data is an array and the first element contains the IP address
+        const firstStuff = response.data[0];
+        if (firstStuff && firstStuff.ip_address) {
+          setIpAddress(firstStuff.ip_address);
+        } else {
+          console.error('Error fetching IP address from localhost:3002/stuffs');
+        }
+      } catch (error) {
+        console.error('Error fetching IP address:', error);
+      }
+    };
+
+    fetchIpAddress();
+  }, []); // Empty dependency array to ensure the effect runs only once on component mount
 
   const updateSettings = async () => {
     try {
-      const response = await axios.post('http://192.168.18.30/setparams', {
+      const response = await axios.post(`http://${ipAddress}/setparams`, {
         limit,
         switch: switchState ? 0 : 1,
         time,
@@ -44,7 +64,9 @@ const Settings = () => {
         onChange={(e) => setTime(parseInt(e.target.value, 10))}
       />
 
-      <button onClick={updateSettings}>Update Settings</button>
+      <button onClick={updateSettings} disabled={!ipAddress}>
+        Update Settings
+      </button>
     </div>
   );
 };
