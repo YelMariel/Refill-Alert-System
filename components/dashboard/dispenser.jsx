fd971ce8d5fe6904ad3ebfec6f03a728ef505dd2
yel <118788188+YelMariel@users.noreply.github.com>
2023-11-22 00:44:39 +0800

    Update dispenser.jsx

--- components/dashboard/dispenser.jsx
+++ components/dashboard/dispenser.jsx
@@ -2,19 +2,39 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import styles from '../styles/Home.module.css';
 
+
 const Disp = ({ data }) => {
   if (!data || (!Array.isArray(data) && !Array.isArray(data.data))) {
     // Handle the case where data is not an array (you might want to customize this based on your requirements)
     return <p>No data available</p>;
   }
 
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`http://localhost:3001/users/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      const updatedData = Array.isArray(data)
+        ? data.filter((item) => item.id !== id)
+        : data.data.filter((item) => item.id !== id);
+  
+      setData(updatedData);
+    } catch (error) {
+      console.error('Error deleting data:', error);
+    }
+  };
+
   const dataArray = Array.isArray(data) ? data : data.data;
 
-  return (
+ return (
     <div className="col-lg-6 col-md-12">
       <div className="row">
         {dataArray.map((value) => (
-          <div className="col-lg-4 col-md-6 mb-4" key={value.id}>
+          <div className="col-lg-5 col-md-6 mb-4" key={value.id}>
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title"></h5>
@@ -22,19 +42,19 @@ const Disp = ({ data }) => {
                   <strong>Status:</strong>{" "}
                   <span id="status"></span>
                   <div className="d-flex justify-content-between mt-1">
-                  <p>HIGH</p>
-                  {/* Indicator for HIGH data */}
-                  <div style={{ backgroundColor: value.water_level === "HIGH" ? "green" : "transparent", width: "50px", height: "20px", borderRadius: "50%", border: "2px solid darkgreen" }}></div>
-                  <p>LOW</p>
-                  {/* Indicator for LOW data */}
-                  <div style={{ backgroundColor: value.water_level === "LOW" ? "red" : "transparent", width: "50px", height: "20px", borderRadius: "50%", border: "2px solid darkred" }}></div>
-                </div>
+                    <p>HIGH</p>
+                    {/* Indicator for HIGH data */}
+                    <div style={{ backgroundColor: value.water_level === "HIGH" ? "green" : "transparent", width: "50px", height: "20px", borderRadius: "50%", border: "2px solid darkgreen" }}></div>
+                    <p>LOW</p>
+                    {/* Indicator for LOW data */}
+                    <div style={{ backgroundColor: value.water_level === "LOW" ? "red" : "transparent", width: "50px", height: "20px", borderRadius: "50%", border: "2px solid darkred" }}></div>
+                  </div>
                 </div>
                 <div className="card-text">
                   <strong>Consumed:</strong>{" "}
                   <span id="label">{value.consumed}</span>
                 </div>
-                
+                <div style={{ display: 'flex', flexDirection: 'column' }}> <button onClick={() => handleDelete(value.id)}>Delete</button> </div>
               </div>
             </div>
           </div>
@@ -43,6 +63,7 @@ const Disp = ({ data }) => {
     </div>
   );
 };
+
 const Home = () => {
   const router = useRouter();
 
@@ -102,6 +123,7 @@ const Home = () => {
         {/* Display total consumed data outside the Disp component */}
         <div className="col-lg-6 col-md-12">
           <h2>Total Consumed: {totalConsumed}</h2>
+
         </div>
       </div>
     </div>
