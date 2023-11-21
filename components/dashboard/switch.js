60477850893adc1a3222f97e38200da5b69a93d9
yel <118788188+YelMariel@users.noreply.github.com>
2023-11-21 14:44:53 +0800

    Update switch.js

--- components/dashboard/switch.js
+++ components/dashboard/switch.js
@@ -1,26 +1,74 @@
-import React, { useState } from 'react';
+// src/Settings.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function ToggleSwitch() {
-  const [isSwitchOn, setIsSwitchOn] = useState(false);
+const Settings = () => {
+  const [limit, setLimit] = useState(3);
+  const [switchState, setSwitchState] = useState(false);
+  const [time, setTime] = useState(0);
+  const [ipAddress, setIpAddress] = useState('');
 
-  const handleToggle = () => {
-    setIsSwitchOn(!isSwitchOn);
+  useEffect(() => {
+    const fetchIpAddress = async () => {
+      try {
+        const response = await axios.get('http://localhost:3002/stuffs');
+        // Assuming the response data is an array and the first element contains the IP address
+        const firstStuff = response.data[0];
+        if (firstStuff && firstStuff.ip_address) {
+          setIpAddress(firstStuff.ip_address);
+        } else {
+          console.error('Error fetching IP address from localhost:3002/stuffs');
+        }
+      } catch (error) {
+        console.error('Error fetching IP address:', error);
+      }
+    };
+
+    fetchIpAddress();
+  }, []); // Empty dependency array to ensure the effect runs only once on component mount
+
+  const updateSettings = async () => {
+    try {
+      const response = await axios.post(`http://${ipAddress}/setparams`, {
+        limit,
+        switch: switchState ? 0 : 1,
+        time,
+      });
+
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error updating settings:', error);
+    }
   };
 
   return (
-    <div className="form-check form-switch">
+    <div>
+      <label>Limit:</label>
+      <input
+        type="number"
+        value={limit}
+        onChange={(e) => setLimit(parseInt(e.target.value, 10))}
+      />
+
+      <label>Switch:</label>
       <input
         type="checkbox"
-        className="form-check-input"
-        id="toggleSwitch"
-        checked={isSwitchOn}
-        onChange={handleToggle}
+        checked={switchState}
+        onChange={() => setSwitchState(!switchState)}
       />
-      <label className="form-check-label" htmlFor="toggleSwitch">
-        Toggle Switch
-      </label>
+
+      <label>Time:</label>
+      <input
+        type="number"
+        value={time}
+        onChange={(e) => setTime(parseInt(e.target.value, 10))}
+      />
+
+      <button onClick={updateSettings} disabled={!ipAddress}>
+        Update Settings
+      </button>
     </div>
   );
-}
+};
 
-export default ToggleSwitch;
+export default Settings;
