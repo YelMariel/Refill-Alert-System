4c41c2f05c562a9c1afc495a91fa007481ee3131
yel <118788188+YelMariel@users.noreply.github.com>
2023-11-26 14:24:36 +0800

    Update switch1.jsx

--- components/dashboard/switch1.jsx
+++ components/dashboard/switch1.jsx
@@ -3,34 +3,58 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Settings = () => {
-  const [limit, setLimit] = useState(3);
-  const [switchState, setSwitchState] = useState(false);
-  const [time, setTime] = useState(0);
-  const [ipAddress, setIpAddress] = useState('');
+  const [settings, setSettings] = useState([]);
+  const [currentTime, setCurrentTime] = useState(new Date());
+
 
   useEffect(() => {
-    const fetchIpAddress = async () => {
+    const fetchData = async () => {
       try {
         const response = await axios.get('http://localhost:3002/stuffs');
-        // Assuming the response data is an array and the first element contains the IP address
-        const firstStuff = response.data[0];
-        if (firstStuff && firstStuff.ip_address) {
-          setIpAddress(firstStuff.ip_address);
-        } else {
-          console.error('Error fetching IP address from localhost:3002/stuffs');
-        }
+        const data = response.data.map((stuff) => ({
+          ipAddress: stuff.ip_address,
+          location: stuff.location, // Assuming there is a 'location' property in your data
+        }));
+        setSettings(data);
       } catch (error) {
-        console.error('Error fetching IP address:', error);
+        console.error('Error fetching data:', error);
       }
     };
 
-    fetchIpAddress();
-  }, []); // Empty dependency array to ensure the effect runs only once on component mount
+    const updateCurrentTime = () => {
+      setCurrentTime(new Date());
+    };
+
+  fetchData();
+  const intervalId = setInterval(updateCurrentTime, 1000);
+
+  return () => {
+    // Cleanup interval when component unmounts
+    clearInterval(intervalId);
+  };
+}, []);
+
+  return (
+    <div>
+        <div className="clock">{currentTime.toLocaleTimeString()}</div>
+        <div className="cards-container" style={{ display: 'flex', gap: '20px' }}>
+        {settings.map((setting) => (
+            <CardSettings key={setting.ipAddress} setting={setting} />
+        ))}
+        </div>
+  </div>
+  );
+  };
+
+const CardSettings = ({ setting }) => {
+  const { ipAddress, location } = setting;
+  const [switchState, setSwitchState] = useState(false);
+  const [time, setTime] = useState(0);
+
 
   const updateSettings = async () => {
     try {
-      const response = await axios.post(`http://${ipAddress}/setparams`, {
-        limit,
+      const response = await axios.post(`http://${ipAddress}/setparams`, {        
         switch: switchState ? 0 : 1,
         time,
       });
@@ -42,30 +66,27 @@ const Settings = () => {
   };
 
   return (
-    <div>
-      <label>Limit:</label>
-      <input
-        type="number"
-        value={limit}
-        onChange={(e) => setLimit(parseInt(e.target.value, 10))}
-      />
-
+    <div className="card" style={{ width: '250px', height: '200px' }}>
+      <h2>Location:{location}</h2>
       <label>Switch:</label>
       <input
         type="checkbox"
         checked={switchState}
         onChange={() => setSwitchState(!switchState)}
       />
-
-      <label>Time:</label>
-      <input
-        type="number"
-        value={time}
-        onChange={(e) => setTime(parseInt(e.target.value, 10))}
-      />
-
-      <button onClick={updateSettings} disabled={!ipAddress}>
-        Update Settings
+        <label>Time:</label>
+        <input
+          type="number"
+          value={time}
+          onChange={(e) => setTime(parseInt(e.target.value, 10))}
+          style={{ width: '50%' }} // Adjust the width as needed
+        />
+<button
+        onClick={updateSettings}
+        disabled={!ipAddress}
+        style={{ width: '50%', marginTop: '10px' }} // Adjust the width and styling as needed
+      >
+        Set
       </button>
     </div>
   );
